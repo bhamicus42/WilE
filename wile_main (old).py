c5be1db8ea4b11db46f5070090f81a9a48720fe5
bhamicus42 <archetypetheta@gmail.com>
2023-07-30 11:53:52 -0700

    fixing requests
    making efforts to make the requests module work; currently no errors are thrown but the formatting is wrong

--- wile_main (old).py
+++ wile_main (old).py
@@ -22,46 +22,32 @@ DATA_TMP_DIR = os.path.join(DATA_DIR, "tmp")  # where to store temporary data
 import requests  # for accessing HTML-formatted and other web data
 from os.path import join as osJoin  # for concatenating strings to make a URL
 from os import chdir as osChdir  # to change directories, such as when working with data
-# import csv  # for handling API data
-# import json  # for handling API data
-from datetime import datetime  # to mark files with the datetime their data was
-import pandas as pd
+import csv
+from datetime import datetime  # to get dates
 
 SYNOPTIC_API_TOKEN = "eb977b5f24ed48b585ccb4e520906425"
 SYNOPTIC_API_ROOT = "https://api.synopticdata.com/v2/"
 SYNOPTIC_FILTER = "stations/latest"  # TODO: refactor so that this is function argument
 syn_api_req_url = osJoin(SYNOPTIC_API_ROOT, SYNOPTIC_FILTER) # URL to request synoptic data
 syn_api_args = {"token": SYNOPTIC_API_TOKEN, "stid": "KLAX"}  # arguments to pass to the synoptic API
-syn_resp = requests.get(syn_api_req_url, params=syn_api_args)
-syn_resp = syn_resp.json()  # despite it being called json(), this returns a dict object
-# syn_json = json.loads(syn_resp)  # convert the synoptic request to a JSON object
-  # retrieve synoptic response and cast to JSON
-# print(syn_resp)
-# for block in syn_resp:
-#     print(block)
-for line in syn_resp['STATION']:
-    print(line)
+syn_req = requests.get(syn_api_req_url, params=syn_api_args)
+syn_req_dict = syn_req.json()  # convert the synoptic request to a dictionary (despite the function name)
+# TODO: cast this as a json obj from the json module, then get the keys of that obj
 
 # TODO: decompose
 now = datetime.now()  # get current datetime
 now_str = now.strftime("%m.%d.%Y_%H.%M.%S")  # convert the datetime to a string
-syn_csv_filename = "synoptic_request_csv_" + now_str + ".csv"  #
+syn_req_csv_filename = "synoptic_request_csv_" + now_str + ".csv"  #
 
 # write the synoptic request to a CSV file
 osChdir(DATA_TMP_DIR)
 
 # TODO: refactor to use Pandas
-# with open(syn_csv_filename, 'w', newline='') as syn_csv:
-    # syn_fieldnames = syn_req_dict[0].keys()
-    # syn_writer = csv.DictWriter(syn_csv, fieldnames=syn_fieldnames)  # create writer object
-    # syn_writer.writeheader()  # write header to the csv file
-    # syn_writer.writerows(syn_req_dict)
-# syn_df = pd.DataFrame(syn_json)
-syn_df = pd.DataFrame(syn_resp['STATION'])
-with pd.option_context('display.max_rows', None,
-                       'display.max_columns', None,
-                       'display.precision', 3):
-    print(syn_df)
-syn_df.to_csv(syn_csv_filename)
+with open(syn_req_csv_filename, 'w', newline='') as syn_csv:
+    syn_fieldnames = syn_req_dict[0].keys()
+    # syn_fieldnames = syn_req_dict.keys()
+    syn_writer = csv.DictWriter(syn_csv, fieldnames=syn_fieldnames)  # create writer object
+    syn_writer.writeheader()  # write header to the csv file
+    syn_writer.writerows(syn_req_dict)
 
 osChdir(WDIR)
