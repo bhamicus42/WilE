231639563065cdcb337441680b5e93d63192979f
bhamicus42 <archetypetheta@gmail.com>
2023-07-30 17:13:15 -0700

    synoptic working 2
    improving synoptic API request as well as improving formatting for CSV response

--- wile_main.py
+++ wile_main.py
@@ -7,13 +7,16 @@
 # local_variables use lowercase snakecase
 # classMembers use camelcase
 
-# import pandas as pd # for data manipulation
 import os  # TODO: make the directory generation more robust: https://linuxize.com/post/python-get-change-current-working-directory/
+
+# set constants for the main file
 WDIR = os.getcwd()  # current working directory
 DATA_DIR = os.path.join(WDIR, "data")  # where to store data
 DATA_TMP_DIR = os.path.join(DATA_DIR, "tmp")  # where to store temporary data
 
-# TODO: this section is WIP data accessing. Decompose.
+
+
+# TODO: this section is WIP data accessing. Decompose into separate modules.
 
 # TODO
 # WIFIRE historical sets
@@ -24,7 +27,7 @@ from os.path import join as osJoin  # for concatenating strings to make a URL
 from os import chdir as osChdir  # to change directories, such as when working with data
 # import csv  # for handling API data
 # import json  # for handling API data
-from datetime import datetime  # to mark files with the datetime their data was
+# from datetime import datetime  # to mark files with the datetime their data was
 import pandas as pd
 
 
@@ -32,9 +35,11 @@ import pandas as pd
 SYNOPTIC_API_TOKEN = "eb977b5f24ed48b585ccb4e520906425"
 SYNOPTIC_API_ROOT = "https://api.synopticdata.com/v2/"
 SYNOPTIC_FILTER = "stations/latest"  # TODO: refactor so that this is function argument
+SYNOPTIC_RESPONSE_COLUMNS = ["ELEVATION", "LONGITUDE", "QC_FLAGGED", "LATITUDE"]  # TODO: make this a default that can be changed
 syn_api_req_url = osJoin(SYNOPTIC_API_ROOT, SYNOPTIC_FILTER) # URL to request synoptic data
-# syn_api_args = {"token": SYNOPTIC_API_TOKEN, "stid": "KLAX"}  # arguments to pass to the synoptic API
-syn_api_args = {"token": SYNOPTIC_API_TOKEN, "state": "CA"}  # arguments to pass to the synoptic API
+syn_api_args = {"state": "CA", "units": "metric,speed|kph,pres|mb", "varsoperator": "or",
+                "vars": "air_temp,sea_level_pressure",
+                "token": SYNOPTIC_API_TOKEN}  # arguments to pass to the synoptic API
 syn_resp = requests.get(syn_api_req_url, params=syn_api_args)
 syn_resp = syn_resp.json()  # despite it being called json(), this returns a dict object
 # syn_json = json.loads(syn_resp)  # convert the synoptic request to a JSON object
@@ -51,14 +56,21 @@ osChdir(DATA_TMP_DIR)
 
 # syn_df = pd.DataFrame(syn_resp['STATION'])
 syn_df = pd.json_normalize(syn_resp['STATION'])
+syn_dc = syn_df[syn_df.QC_FLAGGED != "TRUE"]  # this removes any row that was flagged for quality control
+syn_df = syn_df[SYNOPTIC_RESPONSE_COLUMNS]  # this removes all columns except the ones in SYNOPTIC_RESPONSE_COLUMNS
 # TODO: filter any QC_FLAGGED = TRUE,
 #       filter unneeded columns
 
+# TODO: decompose
 # with pd.option_context('display.max_rows', None,
 #                        'display.max_columns', None,
 #                        'display.precision', 3):
 #     print(syn_df)
 
 syn_df.to_csv(syn_csv_filename)
+
+# df2 = pd.read_json()
+df = pd.json_normalize(syn_resp['STATION'])
+df.to_csv("test.csv")
 
 osChdir(WDIR)
