ba6eb515c67bf22c524ffc50ca49af7767a3c56c
bhamicus42 <archetypetheta@gmail.com>
2023-07-31 14:49:14 -0700

    continuing synoptic historic
    requests station data one day at a time, but doesn't correctly decrement datetime request. That's to fix next.

--- wile_main.py
+++ wile_main.py
@@ -27,13 +27,14 @@ DATA_DERIVED_DIR = os.path.join(DATA_DIR, "derived")  # where to store derived d
 import requests  # for accessing HTML-formatted and other web data
 from os.path import join as osJoin  # for concatenating strings to make a URL
 from os import chdir as osChdir  # to change directories, such as when working with data
-from datetime import datetime  # to mark files with the datetime their data was pulled
+from datetime import datetime, timedelta  # to mark files with the datetime their data was pulled and to iterate across time ranges
 import pandas as pd
 
 
 # https://api.synopticdata.com/v2/stations/metadata?&state=CA&sensorvars=1&complete=1&token=
 SYNOPTIC_API_TOKEN = "eb977b5f24ed48b585ccb4e520906425"
 SYNOPTIC_API_ROOT = "https://api.synopticdata.com/v2/"
+SYN_TIME_FORMAT = "%Y%m%d%H%M"  # format for time specifiers in synoptic API URLs
 SYNOPTIC_RT_FILTER = "stations/latest"  # filter for real-time data TODO: refactor so that this is function argument
 # this const specifies which columns of the synoptic response to keep
 # TODO: make this a default that can be changed
@@ -93,7 +94,8 @@ print("saved most recent measurements to csv")
 # TODO: decompose this so that it is only run on explicit request.
 
 SYNOPTIC_HIST_FILTER = "stations/timeseries"  # filter for timeseries data TODO: refactor so that this is function argument
-SYN_HIST_START = "199001010000"  # earliest time to seek to is 1990/01/01, 00:00. Most data will be nowhere near that.
+# SYN_HIST_START = "199001010000"  # earliest time to seek to is 1990/01/01, 00:00. Most data will be nowhere near that.
+SYN_HIST_START = "202001010000"  # earliest time to seek to is 2020/01/01, 00:00. Most data will be nowhere near that.
 # this const specifies which columns of the synoptic response to keep
 # TODO: make this a default that can be changed
 # TODO: consider finding a way to keep all observation data
@@ -112,21 +114,49 @@ SYNOPTIC_RESPONSE_COLUMNS = ["ELEVATION", "LONGITUDE", "QC_FLAGGED", "LATITUDE",
                              "OBSERVATIONS.dew_point_temperature_value_1.value",
                              "OBSERVATIONS.relative_humidity_value_1.date_time",
                              "OBSERVATIONS.relative_humidity_value_1.value"]
-syn_hist_end = datetime.now().strftime("%Y%m%d%H%M")  # seek up to the absolute most recent data
+syn_hist_end = datetime.now().strftime(SYN_TIME_FORMAT)  # seek up to the absolute most recent data
 syn_api_hist_req_url = osJoin(SYNOPTIC_API_ROOT, SYNOPTIC_HIST_FILTER)  # URL to request synoptic data
 syn_hist_args = syn_api_args
-syn_hist_args["START"] = SYN_HIST_START
-syn_hist_args["END"] = syn_hist_end
-syn_resp = requests.get(syn_api_hist_req_url, params=syn_hist_args)
-syn_resp = syn_resp.json()  # despite it being called json(), this returns a dict object from the requests module
 
 osChdir(DATA_HIST_DIR)
 #
-now = datetime.now()  # get current datetime
-now_str = now.strftime("%m.%d.%Y_%H.%M.%S")  # convert the datetime to a string
-syn_hist_filename = "synoptic_historical_retrieved_" + now_str + ".csv"
-syn_hist_df = pd.json_normalize(syn_resp)  # ["STATION", "OBSERVATIONS"]
-syn_hist_df.to_csv(syn_hist_filename)
+# now = datetime.now()  # get current datetime
+# now_str = now.strftime("%m.%d.%Y_%H.%M.%S")  # convert the datetime to a string
+# syn_hist_filename = "synoptic_historical_retrieved_" + now_str + ".csv"
+# syn_hist_df = pd.json_normalize(syn_resp)  # ["STATION", "OBSERVATIONS"]
+# syn_hist_df.to_csv(syn_hist_filename)
+
+chunk_range_end = syn_hist_end
+chunk_end_dt = datetime.strptime(chunk_range_end, SYN_TIME_FORMAT)  # temporary datetime object of the chunk range end
+chunk_end_dt -= timedelta(days=1)  # run back one day
+chunk_range_start = chunk_end_dt.strftime(SYN_TIME_FORMAT)
+chunk_start_dt = datetime.strptime(chunk_range_start, SYN_TIME_FORMAT)
+
+start_dt = datetime.strptime(SYN_HIST_START, SYN_TIME_FORMAT)
+range_delta = chunk_end_dt - start_dt  # time difference in complete target range
+min_delta = timedelta(days=1)  # set minimum time delta
+
+while range_delta > min_delta:
+    syn_hist_args["START"] = chunk_range_start
+    syn_hist_args["END"] = chunk_range_end
+
+    syn_resp = requests.get(syn_api_hist_req_url, params=syn_hist_args)
+
+    syn_resp = syn_resp.json()  # despite it being called json(), this returns a dict object from the requests module
+
+    syn_hist_filename = "synoptic_historical_" + chunk_range_start + "-" + chunk_range_end + ".csv"
+    syn_hist_df = pd.json_normalize(syn_resp)  # ["STATION", "OBSERVATIONS"]
+    syn_hist_df.to_csv(syn_hist_filename)
+
+    print("saved " + syn_hist_filename + " to csv")
+
+    chunk_range_end = syn_hist_end
+    chunk_end_dt = datetime.strptime(chunk_range_end, SYN_TIME_FORMAT)  # temporary datetime object of the chunk range end
+    chunk_end_dt -= timedelta(days=1)  # run back one day
+    chunk_range_start = chunk_end_dt.strftime(SYN_TIME_FORMAT)
+    range_delta = chunk_end_dt - start_dt
+
+
 
 print("saved historical measurements to csv")
 
